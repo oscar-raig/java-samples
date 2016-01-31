c51ca891a422b144f9fd29287823484163ebcc11
oscar-raig <oscar.raig@gmail.com>
2016-01-31 18:37:28 +0100

    Adding button and text

--- swing/swing-second-example/src/main/java/raig/org/MainFrame.java
+++ swing/swing-second-example/src/main/java/raig/org/MainFrame.java
@@ -1,13 +1,33 @@
 package raig.org;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame  extends JFrame{
 
     public MainFrame(String title) {
-            super(title);
+        super(title);
 
         setLayout(new BorderLayout());
+
+        final JTextArea textArea = new JTextArea();
+        JButton button = new JButton("Click me!");
+
+        Container container = getContentPane();
+
+        container.add(textArea,BorderLayout.CENTER);
+        container.add(button, BorderLayout.SOUTH);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Hello\n");
+            }
+        });
+
     }
 }
