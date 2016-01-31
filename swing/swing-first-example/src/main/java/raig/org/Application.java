619e5549070eabd3ba7c561fac0ca99b86a1854e
oscar-raig <oscar.raig@gmail.com>
2016-01-31 17:49:42 +0100

    Sample Frame with swing

---
+++ swing/swing-first-example/src/main/java/raig/org/Application.java
@@ -0,0 +1,26 @@
+package raig.org;
+
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Application {
+
+
+  public static void main(String [] args) {
+
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        JFrame jFrame = new JFrame("First Frame");
+
+        jFrame.setSize(500,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+      }
+    });
+
+
+  }
+
+}
