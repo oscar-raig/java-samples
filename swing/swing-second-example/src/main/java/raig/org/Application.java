2bb310b82fe38ff6e3361093e53d7642a0f97712
oscar-raig <oscar.raig@gmail.com>
2016-01-31 18:09:58 +0100

    Adding swing second example

--- swing/swing-second-example/src/main/java/raig/org/Application.java
+++ swing/swing-second-example/src/main/java/raig/org/Application.java
@@ -12,11 +12,11 @@ public class Application {
     SwingUtilities.invokeLater(new Runnable() {
       @Override
       public void run() {
-        JFrame jFrame = new JFrame("First Frame");
+        JFrame jFrame = new MainFrame("First Frame");
 
         jFrame.setSize(500,400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          jFrame.setVisible(true);
+        jFrame.setVisible(true);
       }
     });
 
---
+++ swing/swing-second-example/src/main/java/raig/org/MainFrame.java
@@ -0,0 +1,13 @@
+package raig.org;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class MainFrame  extends JFrame{
+
+    public MainFrame(String title) {
+            super(title);
+
+        setLayout(new BorderLayout());
+    }
+}
