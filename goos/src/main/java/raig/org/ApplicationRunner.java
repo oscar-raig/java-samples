5a8bb1a995d5d4b2a3b3f9079ff18bf9226fd9a6
oscar-raig <oscar.raig@gmail.com>
2016-02-01 07:40:30 +0100

    Chap 11 of Goos

--- goos/src/main/java/raig/org/ApplicationRunner.java
+++ goos/src/main/java/raig/org/ApplicationRunner.java
@@ -1,13 +1,38 @@
 package raig.org;
 
+
+
 public class ApplicationRunner {
+    public static final String SNIPER_ID = "sniper";
+    public static final String SNIPER_PASSWORD = "sniper";
+    private AuctionSniperDriver driver;
+
     public void startBiddingIn(AuctionServer auction) {
+        Thread thread = new Thread("Test Application") {
+            @Override public void run() {
+                try {
+                    Main.main(FakeAuctionServer.XMPP_HOSTNAME,
+                            SNIPER_ID, SNIPER_PASSWORD,
+                            auction.getItemId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+        driver = new AuctionSniperDriver(1000);
+        driver.showsSniperStatus(STATUS_JOINING);
     }
 
     public void showsSniperHasLostAuction() {
+        driver.showsSniperStatus(STATUS_LOST);
     }
 
 
     public void stop() {
+        if (driver != null) {
+            driver.dispose();
+        }
     }
 }
